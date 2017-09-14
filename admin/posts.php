896e6082a5bdfe19b4d6d2f20493fea2064d577f
Jesus Martinez Reynoso <jjmartinez.reynoso@gmail.com>
2017-09-14 16:04:36 -0400

    Modifications to the post program
    Modifications to the post program

--- admin/posts.php
+++ admin/posts.php
@@ -33,8 +33,8 @@
                         
                         switch($source) {
                                 
-                            case '34';
-                            echo "NICE 34";    
+                            case 'add_post';
+                            include "includes/add_post.php";
                             break;    
                                 
                             case '100';
